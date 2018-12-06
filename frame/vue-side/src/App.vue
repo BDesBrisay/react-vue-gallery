2ad33f56878981d1e887a4205f687f8d56bb7b3a
BDesBrisay <bryce@desbrisay.com>
2018-12-05 22:09:57 -0700

    added captions for vue hello wrold and props sections

--- frame/vue-side/src/App.vue
+++ frame/vue-side/src/App.vue
@@ -8,13 +8,13 @@
     />
     <ImageSection 
       title="Hello World Component"
-      caption="this is a caption" 
+      caption="What you are looking at is a basic vue component that displays the text 'Hello World' on the main page of the app. On the left side is the App component that imports HelloWorld then exports it as a component. By doing this you enable the use of the HelloWorld component (shown on the right) anywhere within the '<template>' tags. In order to do this, though, you need to export the HelloWorld component from it's file under the name you wish to reference it as later on." 
       imgName="helloWorld.png"
       background="#eee"
     />
     <ImageSection 
       title="Passing Data Through Props"
-      caption="this is a caption" 
+      caption="One way to get data from the parent component to the child is through the props. By defining the props name in the export, you can then use those keywords to insert the data into your component. There are two different ways you can access these variables in your component and they are either by using double curly brackets around the name of the variable, or you can use the shorthand syntax ':' prefixing an attribute to bind Vue to it and allow Vue to insert the data there." 
       imgName="props.png"
     />
   </div>

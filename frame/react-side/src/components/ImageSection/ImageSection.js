124703b057e1618a5dece78d640eb8971f613be4
BDesBrisay <bryce@desbrisay.com>
2018-12-07 22:13:28 -0700

    added header and image section components to react side

---
+++ frame/react-side/src/components/ImageSection/ImageSection.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// import './ImageSection.css';
+
+class ImageSection extends React.Component {
+  render() {
+    const { title, image, caption, background } = this.props;
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default ImageSection;
